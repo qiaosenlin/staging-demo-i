6def57f824b0f0efc412e2e9717ca7a54d557585
qiaosenlin <qiaosenlin1224@outlook.com>
2021-10-17 11:22:12 -0400

    test

---
+++ client/build/precache-manifest.f6d964e7ed547244381540d6bc7ee745.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07b805bf05bd9de22d198bdfb7509cfa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ed54ecc0bac345855c3e",
+    "url": "/static/css/main.bc9a082a.chunk.css"
+  },
+  {
+    "revision": "9915cd64ef834e3dd26f",
+    "url": "/static/js/2.e683c1e9.chunk.js"
+  },
+  {
+    "revision": "4e4e8b39ecf9882f4c4281fd5207264d",
+    "url": "/static/js/2.e683c1e9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ed54ecc0bac345855c3e",
+    "url": "/static/js/main.dc71b768.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
