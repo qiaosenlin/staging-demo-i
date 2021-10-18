af2da643c16dcd2ed94d8cd6cbe52fca2ad8cf1f
qiaosenlin <qiaosenlin1224@outlook.com>
2021-10-17 22:57:09 -0400

    change title

--- client/src/components/Dashboard/Dashboard.jsx
+++ client/src/components/Dashboard/Dashboard.jsx
@@ -30,10 +30,19 @@ const Dashboard = ({ purchasedStocks }) => {
         <Grid item xs={12} md={5} lg={5}>
         <Paper className={fixedHeightPaper}>
                 tutorial1 --- Rader Chart 
+                <NewlineText />
                 We have built a example tutorial with our client about rader chart. 
+                <NewlineText />
+
                 It was running on the docker container and we failed to deploy it to the web.
+                <NewlineText />
+
                 Will be back in a few weeks.
+                <NewlineText />
+
                 The following link is a temporary presentation to what we suppose to have.
+                <NewlineText />
+
                 <a href="https://github.com/COMP523TEAMI/Online-Peer-Learning-for-Intro-Stats"> Rader Chart </a>
           </Paper>
         </Grid>
